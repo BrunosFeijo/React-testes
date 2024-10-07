62131acc4fe9f567b58fd393eac3472ebaaf9dec
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-10-07 00:33:49 -0300

    jsx testes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ function App() {
 
   const name = 'Bruno'
 
+  function sum(a,b){
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
      <h1>Olá React!</h1>
      <p>Meu primeiro App!</p>
      <h2>Alterando JSX</h2>
      <p>Olá, {name}!</p>
-     <p>Idade: {20 + 10}</p>
+     <p>Idade: {sum(10,20)}</p>
+     <img src={url} alt='Minha imagem' />
     </div>
   );
 }
