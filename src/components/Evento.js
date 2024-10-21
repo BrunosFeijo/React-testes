0e7c3738a827aeeac7d6869ae6190d896e9d96ac
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-10-20 22:45:16 -0300

    evento_por_props

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,12 +1,19 @@
+import Button from "./eventos/Button"
+
 function Evento({ numero }) {
     function meuEvento() {
         console.log(`Evento ativado! ${numero}`)
+    }
+
+    function segundoEvento() {
+        console.log('Ativando segundo evento!')
     }
 
     return (
         <div>
             <p>Clique para disparar um evento:</p>
             <button onClick={meuEvento}>Ativar</button>
+            <Button event={segundoEvento} text="Segundo Evento" />
         </div>
     )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,30 @@
+import { useState } from "react"
+
 function Form() {
     function cadastrarUsuario(e) {
         e.preventDefault() /* para esperar o evento antes de tranferir para o backend */
-        console.log('Cadastrou o usuário!')
+        console.log(`Cadastrou o usuário: ${name}`)
     }   
+
+    const [name, setName] = useState('Bruno') /* useState('default') */
 
     return (
         <div>
             <h1>Meu Cadastro!</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <label htmlFor="name">Nome: </label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite o seu nome" 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" id="password" name="password" placeholder="Digite a sua senha" />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
